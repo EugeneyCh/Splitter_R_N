94ae332878489ce160b56872aa17825050e7538f
EugeneyCh <eugeney@ua.fm>
2023-11-29 14:51:18 +0200

    Update ValidationTextInput.tsx

--- src/components/ValidationTextInput.tsx
+++ src/components/ValidationTextInput.tsx
@@ -1,8 +1,25 @@
 import { Button, ScrollView, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { useState } from "react"
 
-const ValidationTextInput = () => {
+const ValidationTextInput = (props) => {
     const [text, setText] = useState<string>('');
     const [validationMessage, setValidationMessage] = useState<string>('')
+
+    return (
+        <View style={styles.container} >
+            <Text style={styles.msg}>{validationMessage}</Text>
+            <TextInput value={text} />
+        </View>
+    );
 }
 export default ValidationTextInput;
+
+const styles = StyleSheet.create({
+    container: {
+        alignSelf: 'stretch',
+        marginHorizontal: 16,
+    },
+    msg: {
+        color: 'red',
+    },
+});
